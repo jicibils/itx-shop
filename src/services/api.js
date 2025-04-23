93a8447aec0de3cd2408bd344c0aa7cf6d7f9f52
jcibils <jicibils@hotmail.com>
2025-04-23 19:39:44 +0200

    Handle errors in API calls

--- src/services/api.js
+++ src/services/api.js
@@ -3,20 +3,35 @@ import axios from "axios";
 const BASE_URL = "https://itx-frontend-test.onrender.com";
 
 export const getProducts = async () => {
-  const { data } = await axios.get(`${BASE_URL}/api/product`);
-  return data;
+  try {
+    const { data } = await axios.get(`${BASE_URL}/api/product`);
+    return data;
+  } catch (err) {
+    console.error("Error fetching products:", err);
+    throw err;
+  }
 };
 
 export const getProductById = async (id) => {
-  const { data } = await axios.get(`${BASE_URL}/api/product/${id}`);
-  return data;
+  try {
+    const { data } = await axios.get(`${BASE_URL}/api/product/${id}`);
+    return data;
+  } catch (err) {
+    console.error("Error fetching products by id:", err);
+    throw err;
+  }
 };
 
 export const addToCart = async ({ id, colorCode, storageCode }) => {
-  const { data } = await axios.post(`${BASE_URL}/api/cart`, {
-    id,
-    colorCode,
-    storageCode,
-  });
-  return data;
+  try {
+    const { data } = await axios.post(`${BASE_URL}/api/cart`, {
+      id,
+      colorCode,
+      storageCode,
+    });
+    return data;
+  } catch (err) {
+    console.error("Error adding product to cart:", err);
+    throw err;
+  }
 };
